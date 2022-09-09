93a5dd7c0e07d5935810317ac069039e2b7fe046
ger0 <mrgero155@gmail.com>
2022-09-09 15:33:28 +0200

    init

---
+++ app/src/main/java/com/example/btracker/LocationProvider.kt
@@ -0,0 +1,24 @@
+package com.example.btracker
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
+
+class LocationProvider(private val activity: AppCompatActivity) {
+    private val client
+            by lazy { LocationServices.getFusedLocationProviderClient(activity) }
+
+    private val locations = mutableListOf<LatLng>()
+
+    val liveLocation = MutableLiveData<LatLng>()
+
+    fun getUserLocation() {
+        client.lastLocation.addOnSuccessListener { location ->
+            val latLng = LatLng(location.latitude, location.longitude)
+            locations.add(latLng)
+            liveLocation.value = latLng
+        }
+    }
+}
---
+++ app/src/main/java/com/example/btracker/PermissionsManager.kt
@@ -0,0 +1,21 @@
+package com.example.btracker
+
+import android.Manifest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+
+class PermissionsManager(
+    activity: AppCompatActivity,
+    private val locationProvider: LocationProvider) {
+
+    private val locationPermissionProvider = activity.registerForActivityResult(
+        ActivityResultContracts.RequestPermission()) { granted ->
+        if (granted) {
+            locationProvider.getUserLocation()
+        }
+    }
+
+    fun requestUserLocation() {
+        locationPermissionProvider.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+}
