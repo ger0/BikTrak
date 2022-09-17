c5837373afa6c2496b6f007b532ee273356bc963
Николай Чеботаревич <kdiebro13012002@gmail.com>
2022-09-17 14:41:06 +0300

    gradle.build(module) problem

--- app/src/main/java/com/example/biktrak/AuthorizeActivity.kt
+++ app/src/main/java/com/example/biktrak/AuthorizeActivity.kt
@@ -1,13 +1,21 @@
 package com.example.biktrak
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class AuthorizeActivity : AppCompatActivity() {
+    lateinit var inAnimation: Animation
+    lateinit var outAnimation: Animation
 
     val context = this
 
@@ -23,16 +31,25 @@ class AuthorizeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authorize)
 
+        inAnimation = AnimationUtils.loadAnimation(context, R.anim.alpha_in)
+        outAnimation = AnimationUtils.loadAnimation(context, R.anim.alpha_out)
+
         val login = findViewById<TextView>(R.id.login)
         val password = findViewById<TextView>(R.id.Password)
 
+        val img = findViewById<ImageView>(R.id.imageAuthorizeIcon)
+
         val signInButton = findViewById<Button>(R.id.signInButton)
         signInButton.setOnClickListener{
-            onSignInStart(login.text.toString(), password.text.toString())
+            onSignInStart(login.text.toString(), password.text.toString(), img)
         }
     }
 
-    fun onSignInStart(login: String, password: String){
+    fun onSignInStart(login: String, password: String, img:ImageView){
+        val animator = ObjectAnimator.ofFloat(img, View.ROTATION,  0f,360f)
+        animator.duration = 1000
+        animator.start()
+
         if (!isPass(login, password)){
             return
         }
--- app/src/main/java/com/example/biktrak/MainActivity.kt
+++ app/src/main/java/com/example/biktrak/MainActivity.kt
@@ -2,10 +2,66 @@ package com.example.biktrak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.example.biktrak.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+    lateinit var mBinding: ActivityMainBinding
+    lateinit var mDrawer: Drawer
+    lateinit var mHeader: AccountHeader
+    lateinit var mToolbar: Toolbar
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+    }
+
+    private fun initFunc() {
+        setSupportActionBar(mToolbar)
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        mDrawer = DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(mToolbar)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .withAccountHeader(mHeader)
+            .addDrawerItems(
+                PrimaryDrawerItem().withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName("Jazda")
+                    .withSelectable(false)
+
+            ).build()
+    }
+
+    private fun createHeader() {
+        mHeader = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem().withName("Nikolay")
+            ).build()
+    }
+
+    private fun initFields() {
+        mToolbar = mBinding.mainToolbar
     }
 }
